e5e12b421e87aea62ddfb69c99c1afa3a8737d56
Doron Segal <doron2402@gmail.com>
2014-04-11 13:07:18 -0700

    adding script for generating users

--- scripts/generate_users_mongo.js
+++ scripts/generate_users_mongo.js
@@ -1,8 +1,5 @@
-var mongodb = require('mongodb');
-var mongoose = require('mongoose');
+var mongodbObj = require('../lib/mongodb');
 var config = require('../settings/config.json');
-var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
 
 if (process.env.ENV) {
   config = config[process.env.ENV];
@@ -10,40 +7,10 @@ if (process.env.ENV) {
   config = config.dev;
 }
 console.log('Generate users...');
-console.log(config);
-//Connecting to mongodb
-mongoose.connect(config.mongodb.host, config.mongodb.db);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
-  console.log('Connected to DB');
-});
 
-// User Schema
-var userSchema = mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true}
-});
 
-userSchema.pre('save', function(next) {
-  var user = this;
+var User = = mongodbObj.userModel;
 
-  if(!user.isModified('password')) return next();
-
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if(err) return next(err);
-
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if(err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-// Seed a user
-var User = mongoose.model('User', userSchema);
 var users_list = [
 { username: 'doron', email: 'doron@doron.com', password: 'doron' },
 { username: 'amir', email: 'amir@amir.com', password: 'amir' }
@@ -60,7 +27,6 @@ for (var i=0; i< users_list.length; i++) {
 
       if (i == users_list.length) {
         console.log('Complete.');
-        mongoose.connection.close();
         process.exit();
       }
     }
